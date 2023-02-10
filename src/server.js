eb4f420378e1d6c158c976b4301b0ad4d1f70d74
Choseoyoung <seoyoung0000@naver.com>
2023-02-10 15:48:00 +0900

    User Count

--- src/server.js
+++ src/server.js
@@ -27,6 +27,10 @@ function publicRooms(){
     return publicRooms;
 }
 
+function countRoom(roomName){
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection",socket=>{
     socket["nickname"]="Anon";
     socket.onAny((event) => { 
@@ -35,12 +39,12 @@ wsServer.on("connection",socket=>{
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome",socket.nickname);
+        socket.to(roomName).emit("welcome",socket.nickname,countRoom(roomName));
         wsServer.sockets.emit("room_change",publicRooms());
     });
     socket.on("disconnecting",()=>{
         socket.rooms.forEach((room)=>
-            socket.to(room).emit("bye",socket.nickname)
+            socket.to(room).emit("bye",socket.nickname,countRoom(room) - 1)
         );
         wsServer.sockets.emit("room_change", publicRooms());
     });
